f19c1ebe8bec36938ca29debbd1be75cdff2e2a9
RAMILSON Lorys <lorys.ramilson@etu.univ-amu.fr>
2023-06-05 12:15:22 +0200

    DEBUT FXML PAGE PRINCIPALE

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,11 +5,12 @@ module com.example.sae201 {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
+
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+
     requires com.almasb.fxgl.all;
+    requires com.gluonhq.maps;
 
     opens com.example.sae201 to javafx.fxml;
     exports com.example.sae201;
