20e778805dfae43fc71a5a3bbd0e18a25694ca9a
RAMILSON Lorys <lorys.ramilson@etu.univ-amu.fr>
2023-06-05 10:37:08 +0200

    INITIAL COMMIT

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.sae201 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.sae201 to javafx.fxml;
+    exports com.example.sae201;
+}
